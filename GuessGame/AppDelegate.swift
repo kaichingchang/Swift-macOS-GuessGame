a0688958de129815e37fac0b4bb313922787bb6c
張凱慶 <kaiching@zhangkaiqingdeMacBook-Pro.local>
2017-08-01 08:29:09 +0800

    Initial Commit

---
+++ GuessGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GuessGame
+//
+//  Created by 張凱慶 on 01/08/2017.
+//  Copyright © 2017 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GuessGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GuessGame
+//
+//  Created by 張凱慶 on 01/08/2017.
+//  Copyright © 2017 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
